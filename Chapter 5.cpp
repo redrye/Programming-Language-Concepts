2c6cc1e1ff5a3e07e0eda4913749750cab385e49
Ryan Boudreaux <boudreauxryan@gmail.com>
2018-02-26 12:23:01 -0600

    Update Chapter 5.cpp

--- Chapter 5.cpp
+++ Chapter 5.cpp
@@ -1,2 +1,37 @@
 #include <stdio.h>
 #include <iostream>
+
+class A { 
+public: 
+  virtual void p() {
+    cout << “A.p” << endl;
+  } 
+  void q() {
+    cout << “A.q” << endl;
+  }
+  virtual void r() {
+    p();
+    q();
+  } 
+}; 
+class B : public A { 
+public: 
+  void p() { 
+    cout << “B.p” << endl;
+  } 
+}; 
+class C : public B {
+public: 
+  void q() {
+    cout << “C.q” << endl;
+  }
+  void r() {
+    p();
+    q();
+  } 
+};
+
+int main() {
+  
+  return 0;
+}
